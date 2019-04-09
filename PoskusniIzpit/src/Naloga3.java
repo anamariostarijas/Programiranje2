37521ba6fac391c4070ec78ee573bd28d4d723d2
Anamari Oštarijaš <anamari.ostarijas@gmail.com>
2019-04-09 23:48:31 +0200

    poprava naloge

--- PoskusniIzpit/src/Naloga3.java
+++ PoskusniIzpit/src/Naloga3.java
@@ -9,21 +9,21 @@ public class Naloga3 {
 	
 	public static void ocene(String imeVhod) throws IOException{
 		BufferedReader vhod = new BufferedReader(new FileReader(imeVhod));
-		int[] ocene = new int[9];
+		int[] ocene = new int[10];
 		while (vhod.ready()) { 
 			String vrstica = vhod.readLine().trim();
 			if (vrstica.equals("")) continue; 
 			String[] info = vrstica.split(" "); 
 			int tocke = Integer.parseInt(info[2]);
-			int ocena = (int) Math.ceil((tocke+1)/10);
+			int ocena = (int) Math.ceil((tocke+1)*0.1);
 			if (ocena == 11) ocena = 10;
 			ocene[ocena-1] += 1;
 		}
-		for (int i=0; i<8; i++) {
+		for (int i=0; i<9; i++) {
 			int j = i+1;
 			System.out.println(" " + j + ": " + String.join("", Collections.nCopies(ocene[i], "*")));
 		}
-		System.out.println("10" + ": " + String.join("", Collections.nCopies(ocene[8], "*")));
+		System.out.println("10" + ": " + String.join("", Collections.nCopies(ocene[9], "*")));
 
 		vhod.close();
 		
